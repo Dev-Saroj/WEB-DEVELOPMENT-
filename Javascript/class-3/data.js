44a7542c34d0883d855965d3845ea6462c2b3473
Saroj Roul <sarojroul300@gmail.com>
2024-09-03 00:09:21 +0530

    pending done

--- Javascript/class-3/data.js
+++ Javascript/class-3/data.js
@@ -46,7 +46,7 @@ let decimal_no = 0.0001;
 console.log(typeof decimal_no); // number
 
 // Task-1:
-// Do mathematical operations to perform addition, subtraction, multiplication and divison using "number" data types in js.
+//// todo: Do the above task and upload it in github then share me the link in the group.
 
 // Addition :
 let num1 = 400;
@@ -71,3 +71,129 @@ let num7 = 60;
 let num8 = 5;
 let divison_result = num7 / num8;
 console.log(divison_result); //12
+
+// BigInt
+//BigInt is a primitive data type in javascript.It is used to represent a whole number that is 
+//Larger than the maximum limit of the Number data type. It is used to represent larger integers.
+
+let bigInt = 10000000000000000000000000000000000000000000000000000000000000000000000000;
+// console.log(bigInt); //1e+73
+
+// Symbol :
+// Symbol is a primitive data type in JavaScript. It is used to create a unique identifier for an
+// object's property. It is used to prevent property name collisions.
+
+let symbol1 = Symbol("abc");
+let symbol2 = Symbol("abc");
+// console.log(symbol1); // Symbol(abc)
+// console.log(symbol2); // Symbol(abc)
+// console.log(symbol1.description); // abc
+// console.log(symbol2.description); // abc
+// console.log(symbol1 === symbol2); // false
+
+// NaN : Not a Number
+// NaN is a primitive data type in JavaScript. It is used to represent a value that is not
+// a number. It is used to represent the result of a mathematical operation that cannot be
+// performed.
+
+let num3 = 10 / 0;
+// console.log(num3); // NaN / Infinity
+
+// Boolean :
+// Boolean is a primitive data type in JavaScript. It is used to represent a logical value that
+// can be either true or false.
+
+let boolean1 = true;
+let boolean2 = false;
+// console.log(boolean1);
+// console.log(boolean2);
+
+let boolean3 = " ";
+// boolean3 ? console.log("true") : console.log("false");
+
+// String :
+// String is a primitive data type in JavaScript. It is used to represent a sequence of characters.
+// collections of characters.
+// It is used to represent text data.
+
+// How to define
+// 1. Using single quotes
+let str1 = "Hello, World!";
+// console.log(str1);
+
+// 2. Using double quotes
+let str2 = "Hello, World!";
+// console.log(str2);
+
+// 3. Using backticks (template literals)
+let str3 = `Hello, World!`;
+// console.log(str3);
+
+// 4. Using new String()
+let str4 = new String("Hello, World!");
+// console.log(str4);
+
+// My Details :
+// Name : saroj
+// Age : 22
+// Address : Bhubaneswar
+// Contact : 1234567890
+
+let name = "Saroj";
+let age = "22";
+let address = "Bhubaneswar";
+let contact = "1234567890";
+
+console.log(name);
+console.log(age);
+console.log(address);
+console.log(contact);
+
+console.log("My details :", name, age, address, contact); // My details : Saroj 22 Bhubaneswar 1234567890
+console.log("My details:" + name + age + address + contact); // My details:Saroj22Bhubaneswar1234567890
+console.log(
+  `My name is ${name}, my age is ${age}, my address is ${address} and my contact no is ${contact}`
+);
+
+// My name is Saroj, my age is 22, my address is Bhubaneswar and my contact no is 1234567890
+
+// Fun fact :
+let score1 = "100";
+let score2 = 200;
+
+let number = "Saroj";
+console.log(typeof number); // string
+
+let string = 5.23;
+console.log(typeof string); // number
+
+let name_string = "Pablo";
+// length: 5
+// Start-index: 0
+// Last-Index = 5 - 1 = 4
+console.log(name_string.length); // 5
+console.log(`Last index: ${name_string.length - 1}`); // Last index: 4
+console.log(name_string[2]); // b
+console.log(name_string[4]); // o
+console.log(name_string[name_string.length - 1]); // o
+console.log(name_string.toUpperCase()); // PABLO
+console.log(name_string.__proto__); // {}
+console.log(name_string.__proto__.__proto__); // {}
+console.log(name_string.charAt(2)); // b
+console.log(name_string.indexOf("o")); // 4
+
+const url = "sarojroul.github.io";
+console.log(url.replace(".", "-")); // sarojroul-github.io
+const email = "saroj@gmail@yahoo@ios.com";
+console.log(email.replace("@", ".")); // saroj.gmail@yahoo@ios.com
+
+const mail = "sarojroul.com";
+console.log(mail.includes(".com")); // true
+console.log(mail.includes("@yahoo")); // false
+
+const company_name = new String("WeCode");
+console.log(company_name); // [String: 'WeCode']
+
+
+
+
---
+++ Javascript/class-4/conversion.js
@@ -0,0 +1,74 @@
+"use strict"
+// use strict : To enable strict mode in javascript, you can use the stict directive. This directive can be placed at the top of a script or funtion to opt-in to strict mode.
+
+//conversion:
+let score = "saroj"
+//console.log(typeof score); //string
+
+// 1. convert string string to number :
+let converted_score = Number(score);
+// console.log(converted_score); //NaN(Not a Number)
+
+// 2. alternate method :
+let converted_score2 = parseInt("score");
+// console.log(converted_score2); // NaN(Not a Number is data type of Number)
+
+let nums = "100"; // string
+let converted_nums = parseInt(nums); // convert string to number
+// console.log(typeof converted_nums); // number
+
+// 3. alternate way :
+let converted_nums2 = +nums;
+// console.log(typeof converted_nums2); // number
+
+// Task-1:
+// let num = "123.45";
+// Convert the above `num` to `Number` data type.
+
+let num = "123.45"; // string
+// console.log(typeof num); // string
+let converted_num = Number(num); // convert string to number
+// console.log(typeof converted_num); // number
+
+let converted_nums3 = parseInt(num);
+// console.log(typeof converted_nums3); // number
+
+let converted_num4 = +num;
+// console.log(typeof converted_num4); // number
+
+// Convert Number to String :
+let num1 = 123.45; // number
+
+// console.log(typeof num1); // number
+let converted_num1 = String(num1); // convert number to string
+// console.log(typeof converted_num1); // string
+
+// or
+let converted_num2 = num1.toString();
+// console.log(typeof converted_num2); // string
+
+// or
+let converted_num3 = num1 + ""; // concatenation
+// console.log(typeof converted_num3); // string
+
+// Fun :
+let num5 = 91;
+let num6 = 21;
+let num7 = "19";
+// console.log(num5 + num6); // 112
+// console.log(num5 + num6 + num7); // 11219
+
+// Comparison :
+console.log(2 > 1); // true
+console.log(100 < 200); // true
+console.log(100 > 200); // false
+console.log(2 == 1); // false
+console.log(2 === 4); // false
+console.log(2 == "2"); // true
+console.log(2 === "2"); // false (Recommended)
+console.log("000002" > 0.0001); //true
+console.log(null > 0); // false
+console.log(null === 0); // false
+console.log(null == 0); // false
+
+
