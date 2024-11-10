ee864ad4384f9d625340475caefa44c973760a16
Saroj Roul <sarojroul300@gmail.com>
2024-11-10 22:19:40 +0530

    done

---
+++ React/class-1/react-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="nav">
+        <li>Home</li>
+        <li>About</li>
+        <li>Service</li>
+        <li>Auth</li>
+      </nav>
+      <main className="main">
+        <h1>I am learning React</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
